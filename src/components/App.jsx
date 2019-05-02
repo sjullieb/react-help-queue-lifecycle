d88b09d30f9a68592ac31307c600974d841086f0
sjull <sjullieb@gmail.com>
2019-05-01 23:36:18 -0700

    explore components lifecycle

---
+++ src/components/App.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+import Moment from 'moment';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+  
+  componentDidMount(){
+    console.log('componentDidMount');
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(), 5000
+    );
+  }
+  
+  componentWillMount(){
+    console.log('componentWillMount');
+  }
+  
+  componentWillReceiveProps(){
+    console.log('componentWillReceiveProps');
+  }
+  
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+  
+  componentWillUpdate(){
+    console.log('componentWillUpdate');
+  }
+  
+  componentDidUpdated(){
+    console.log('componentDidUpdated');
+  }
+  
+  updateTicketElapsedWaitTime(){
+    console.log('check');
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+  
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Ticket(props){
+  return (
+    <div>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <p>{props.formattedWaitTime}</p>
+      <hr/>
+    </div>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string.isRequired
+};
+
+export default Ticket;
